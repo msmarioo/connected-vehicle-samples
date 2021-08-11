68c60581e46a477661e4d3af5127e63b508051ae
Mario Ortegon Cabrera <marioo@microsoft.com>
2021-08-11 11:48:05 +0200

    fixed call params entry point

--- samples/workloads/tools/VehicleReplayTool/src/EntryPoint.py
+++ samples/workloads/tools/VehicleReplayTool/src/EntryPoint.py
@@ -8,11 +8,9 @@ port = 1883
 
 client_id = "VehicleReplayComponent_LP1007"
 
-#mdf4_folder = "/home/azureUser/mdf4/"
 mdf4_folder = os.getcwd() +"/"
 
 mdf4_filename = mdf4_folder + sys.argv[2] 
-#mdf4_filename = "/home/azureUser/mdf4/normaldriving_201125_104306.mf4"
 
 log_filename = "VehicleReplayComponent.log"
 log_level = logging.INFO
