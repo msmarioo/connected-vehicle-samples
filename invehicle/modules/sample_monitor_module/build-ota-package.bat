ac77326677eb414adef0edf7160798288b516fc1
msmarioo <mario.ortegon@microsoft.com>
2021-08-26 13:08:26 +0000

    setup the build of the OTA package

--- invehicle/modules/sample_monitor_module/build-ota-package.bat
+++ invehicle/modules/sample_monitor_module/build-ota-package.bat
@@ -1,11 +1,23 @@
+@echo off
+
 REM built the docker image
-docker build -t samplemonitormodule:0.0.1 .
+docker build -f docker\linux\amd64\Dockerfile -t samplemonitormodule:0.0.1 src/
+
+REM Make the build directories
+mkdir tmp
+mkdir build
+cd tmp
+mkdir samplemonitormoduleSoftwarePackage
 
 REM Exporting the docker image
 docker save --output samplemonitormoduleSoftwarePackage/samplemonitormodule.tar samplemonitormodule
 
 REM Building the TAR.GZ File
-if not exists build\NUL mkdir build
-tar -czvf tripandmaneuvermodule.tar.gz tripandmaneuverSoftwarePackage
+tar -czvf samplemonitormodule.tar.gz samplemonitormoduleSoftwarePackage
 
-pause
+REM Moving the prepared OTA package to the build folder
+move samplemonitormodule.tar.gz ../build/samplemonitor.tar.gz
+
+REM Delete the temp folder
+cd ..
+rmdir /S /Q tmp
